9e32d4ebf5c7d541d073c4df54ac4ecaa7e6821f
Adarsh Pandey <adarshpandey2342003@gmail.com>
2024-02-11 01:51:40 +0530

    Add signout functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import { Sidebar } from 'flowbite-react'
+import { Sidebar } from 'flowbite-react';
 import {
-    HiUser,
-    HiArrowSmRight,
-    HiDocumentText,
-    HiOutlineUserGroup,
-    HiAnnotation,
-    HiChartPie,
-  } from 'react-icons/hi';
-  import { useEffect, useState } from 'react';
+  HiUser,
+  HiArrowSmRight,
+} from 'react-icons/hi';
+import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 
@@ -25,26 +21,47 @@ export default function DashSidebar() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
-        <Link to='/dashboard?tab=profile'>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
+
+          <Link to='/dashboard?tab=profile'>
             <Sidebar.Item
               active={tab === 'profile'}
               icon={HiUser}
-              label={'User'}
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
               labelColor='dark'
               as='div'
             >
               Profile
             </Sidebar.Item>
           </Link>
-          <Sidebar.Item  icon={HiArrowSmRight} className = 'cursor-pointer'>
+          
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className='cursor-pointer'
+            onClick={handleSignout}
+          >
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
 
-function Header() {
-    const path = useLocation().pathname;
-    const { currentUser } = useSelector((state) => state.user);
-    const dispatch = useDispatch();
-    const { theme } = useSelector((state) => state.theme);
+export default function Header() {
+  const path = useLocation().pathname;
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <>
-    <Navbar>
-    <Link
+    <Navbar className='border-b-2'>
+      <Link
         to='/'
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-          Adarsh's
+          Sahand's
         </span>
         Blog
       </Link>
-
-      <form>
+      <form >
         <TextInput
           type='text'
           placeholder='Search...'
@@ -35,16 +53,15 @@ function Header() {
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
-   
-
-    <div className='flex gap-2 md:order-2'>
+      <div className='flex gap-2 md:order-2'>
         <Button
           className='w-12 h-10 hidden sm:inline'
           color='gray'
           pill
-        onClick={()=>{dispatch(toggleTheme())}}>
-           {theme === 'light' ? <FaMoon /> : <FaSun />}
-        </Button>  
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        </Button>
         {currentUser ? (
           <Dropdown
             arrowIcon={false}
@@ -63,7 +80,7 @@ function Header() {
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            <Dropdown.Item >Sign out</Dropdown.Item>
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to='/sign-in'>
@@ -72,10 +89,10 @@ function Header() {
             </Button>
           </Link>
         )}
-          <Navbar.Toggle />
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-      <Navbar.Link active={path === '/'} as={'div'}>
+        <Navbar.Link active={path === '/'} as={'div'}>
           <Link to='/'>Home</Link>
         </Navbar.Link>
         <Navbar.Link active={path === '/about'} as={'div'}>
@@ -85,9 +102,6 @@ function Header() {
           <Link to='/projects'>Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
-      </Navbar>
-    </>
-  )
+    </Navbar>
+  );
 }
-
-export default Header
