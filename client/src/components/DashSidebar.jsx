c344e80782cdf5d07671f0ffef66992b07f6a001
Adarsh Pandey <adarshpandey2342003@gmail.com>
2024-02-13 09:01:41 +0530

    Show user posts inside dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -3,6 +3,7 @@ import {
   HiUser,
   HiArrowSmRight,
   HiDocumentText,
+  HiOutlineUserGroup,
 } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
@@ -63,6 +64,20 @@ export default function DashSidebar() {
                 Posts
               </Sidebar.Item>
             </Link>
+          )}
+            {currentUser.isAdmin && (
+            <>
+              <Link to='/dashboard?tab=users'>
+                <Sidebar.Item
+                  active={tab === 'users'}
+                  icon={HiOutlineUserGroup}
+                  as='div'
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+              
+            </>
           )}
          
           <Sidebar.Item
